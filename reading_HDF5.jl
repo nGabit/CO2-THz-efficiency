0f3827db4ce4f1e0d892608ed5c7d9b797e1333b
nGabit <118909517+nGabit@users.noreply.github.com>
2025-02-06 22:57:04 +0100

    Update reading_HDF5.jl

--- reading_HDF5.jl
+++ reading_HDF5.jl
@@ -3,24 +3,15 @@ using HDF5
 using Plots
 
 file_path = raw"\\synology-fiz\illesg-volume\2d-calculations\gpu-test-ZnTe-50GW-0.5ps-wide.hdf5"
-
 readH5 = h5open(file_path, "r") # read 
-
 println("data in the file:", readH5)  # List datasets in the file
 #close(readH5)
-
-# Access specific datasets in terminal
-# read(readH5["gamma"])   #or
-# h5read(file_path, "/gamma")
-
 
 FID = readH5
 function geteffic(FID)
     En0 = sum(read(FID["/En"])[1, :])
     EnTHz = sum(read(FID["/EnTHz"]), dims=2)
-   
     effic = EnTHz ./ En0
-   
     #effic = dropdims(effic, dims=2) * 100 * 4 * nTHzo(1.5e12 * 2 * pi, 300, 4) ./ (1 + nTHzo(1.5e12 * 2 * pi, 300, 4)) .^ 2
     return effic
    
